1215223ecedf48944a357e5b56de3c55805ddd68
Jofreylin Perez <trabajosjofrey@hotmail.com>
2023-11-25 11:44:02 -0400

    inital, first structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './utils/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    data: {expectedRole: 'ADMIN'},
+    children: [
+      {
+        path: 'admin',
+        loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)
+      }
+    ]
+  },
+  {
+    path: 'guess',
+    loadChildren: () => import('./pages/guest/guest.module').then(m => m.GuestModule)
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'account'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS, HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { JwtModule } from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
+import { DatePipe } from '@angular/common';
+import { AuthGuard } from './utils/auth.guard';
+import { CustomHttpInterceptor } from './utils/custom-http.interceptor';
+
+export function tokenGetter() {
+  return localStorage.getItem("token");
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +24,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgxSpinnerModule,
+    AppRoutingModule,
+    ToastrModule.forRoot(),
+    NgxMaskDirective, 
+    NgxMaskPipe,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: [environment.API_DOMAIN],
+        disallowedRoutes: [],
+      }
+    }),
   ],
-  providers: [],
+  providers: [provideNgxMask(),DatePipe,ToastrService, AuthGuard, {provide: HTTP_INTERCEPTORS, useClass: CustomHttpInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
